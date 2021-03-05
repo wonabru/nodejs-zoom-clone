9563a713e1003faf14c3c25d226ce66714deecd9
wonabru <wonabru@gmail.com>
2021-03-05 18:23:56 +0100

    to change

--- public/script.js
+++ public/script.js
@@ -6,14 +6,14 @@ const myPeer = new Peer(undefined, {
   port: '30303'
 })
 
-var crypto = io('/crypto')
+const crypto = io('/crypto')
 
 const algorithm = 'aes-256-ecb'
 const password = '12345678901234567890123456789012'
 
-var encrypt = crypto.createCipher(algorithm, password);
+//var encrypt = crypto.createCipher(algorithm, password)
 
-var decrypt = crypto.createDecipher(algorithm, password);
+//var decrypt = crypto.createDecipher(algorithm, password)
 
 //var crypto = require('crypto'),
 //    algorithm = 'aes-256-crt',
