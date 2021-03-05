f53cd962f6c73afc832ee043ab147570eca97000
wonabru <wonabru@gmail.com>
2021-03-06 00:38:49 +0100

    to change

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ var iv = [ 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,35, 36 ];
 
 var aesCbc = new aesjs.ModeOfOperation.cbc(key, iv);
 
-const scramjet = require('scramjet-core').scramjet
+//const scramjet = define('scramjet')
 
 function enrypting(aesCbc, input) {
 const output = scramjet.DataStream.from(async function* () {
